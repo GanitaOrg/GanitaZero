228dfdef55685894150a79fe968abc0093afb065
terry <terry@ganita.org>
2014-12-30 09:37:20 -0500

    Version 0.1.2 - patch for clang on Mac

--- src/cpp/GanitaZeroVersion.cpp
+++ src/cpp/GanitaZeroVersion.cpp
@@ -6,7 +6,7 @@
 GanitaZeroVersion::GanitaZeroVersion(void)
 {
   // This is the main required place to set version numbers. 
-  version = "0.1.1";
+  version = "0.1.2";
 }
 
 std::string GanitaZeroVersion::returnVersion(void)
--- src/cpp/representations/GanitaZeroAdic.hpp
+++ src/cpp/representations/GanitaZeroAdic.hpp
@@ -10,7 +10,7 @@ private:
   std::string name;
   std::vector<std::string> type;
   std::vector<std::string> keys;
-  std::vector<std::vector<std::string>> values;
+  std::vector< std::vector<std::string> > values;
 public:
   GanitaZeroAdic(void);
   int readAdic(std::ifstream &gzt_file);
