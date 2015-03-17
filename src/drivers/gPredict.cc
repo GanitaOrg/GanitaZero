93f9ec91c2f871393353d858248ba646be6e9391
terry <terry@ganita.org>
2015-03-17 19:48:56 -0400

    Version 0.7.0 - added driver program gPredict and beginning methods for analyzing double floating point time series

---
+++ src/drivers/gPredict.cc
@@ -0,0 +1,51 @@
+#include "ganita/zero/GanitaZero.hpp"
+  
+// driver program for GanitaZero. 
+
+int main(int argc, char *argv[])
+{
+  GanitaZero gzero(1);
+  int ii;
+  double entropy[32][2];
+  double max;
+  int max_ii;
+
+  if(argc < 3){
+    std::cout<<"Usage: input-file output-file"<<std::endl;
+    exit(1);
+  }
+
+  //gzero.readT(argv[1]);
+  //cout<<"Number of stages:        "<<gzero.returnNumStages()<<endl;
+  //gzero.dumpStage(1);
+  gzero.initD(argv[1]);
+  gzero.loadDoubleDiffQuant();
+  //gzero.computeByteHist();
+
+  max = 0;
+  max_ii = 4;
+  entropy[4][0] = gzero.computeCondEnt1FromScratch(4);
+  for(ii=5; ii<25; ii++){
+    entropy[ii][0] = gzero.computeCondEnt1FromScratch(ii);
+    entropy[ii][1] = (entropy[ii-1][0] - entropy[ii][0]) / entropy[ii][0];
+    fprintf(stdout, "Bits %2d C_Entropy %lf Reduction %lf\n", 
+	    ii, entropy[ii][0], entropy[ii][1]);
+    if(entropy[ii][1] > max){
+      max_ii = ii-1;
+      max = entropy[ii][1];
+    }
+    else if(max_ii > 4){
+      // Just look for first local max.
+      break;
+    }
+  }
+
+  //fprintf(stdout, "Conditional bits: %d\n", max_ii);
+  gzero.computeCondEnt1FromScratch(max_ii);
+  //gzero.dumpCondHistSep();
+  //gzero.dumpCondHist();
+
+  //cout<<"Base64 encoding: "<<gzero.returnB64Encode()<<endl;
+  gzero.close();
+}
+
